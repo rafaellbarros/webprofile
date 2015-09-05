13b409101f1b1a41d48a728a233727f830fb0fb4
Rafael Barros <rafaelbarros.df@gmail.com>
2015-09-05 01:09:04 -0300

    Definindo a estrutura

--- index.php
+++ index.php
@@ -2,13 +2,93 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
-	<title>WebCode</title>
+	<title>WebCode - Rafael Barros</title>
 
 	<!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
 	<![endif]-->
+
+	<link rel="stylesheet" href="css/oocss.css" />
 </head>
 <body>
+	<header class="container bg-gray">
+		<div class="content">
+			<h1 class="main_logo">
+				<a title="" href="#index" >
+					WebCode - Rafael Barros
+				</a> 
+			</h1>
+
+			<ul class="main_nav">
+				<li><a title="About" href="#">About</a></li>
+				<li><a title="Work" href="#">Work</a></li>
+				<li><a title="Contact" href="#">Contact</a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</header>
+
+	<!--CONTEUDO-->
+	<article class="container">
+		<div class="content">
+			<h1>Welcome</h1>
+			<h2>Hello, I'am Rafael a  Web Developer</h2>
+			
+			<div class="clear"></div>
+		</div>
+	</article>
+
+	<article class="container bg-blue">
+		<div class="content">
+			<h1>Abilities</h1>
+			<h2>html</h2>
+			<h2>css</h2>
+			<h2>javascript</h2>
+			<h2>jquery</h2>
+			<h2>bootstrap</h2>
+			<h2>angularJs</h2>
+			<h2>php</h2>
+			<div class="clear"></div>
+		</div>
+	</article>
+
+	<!--CONTEUDO-->
+
+	<footer class="container bg-light">
+		<section class="content">
+			<h1 class="fontzero">Sobre WebCode</h1>
+
+			<nav class="main_nav">
+				<h1 class="title">Outros Links</h1>
+				<ul>
+					<li><a title="" href="#">link1</li>
+					<li><a title="" href="#">link2</li>
+					<li><a title="" href="#">link3</li>
+					<li><a title="" href="#">link4</li>
+				</ul>	
+			</nav>
+
+			<article class="main_social">
+				<h1 class="title">WebCode - Redes sociais:</h1>
+				<ul>
+					<li><a target="_blank" rel="nofollow" title="Facebook" href="#">Facebook</li>
+					<li><a target="_blank" rel="nofollow" title="Google" href="#">Google+</li>
+					<li><a target="_blank" rel="nofollow" title="Linkedin" href="#">Linkedin</li>
+					<li><a target="_blank" rel="nofollow" title="" href="Twitter">Twiter</li>
+				</ul>	
+			</article>
+
+			<article class="main_copy">
+				<h1 class="fontzero">Plataforma WebCode</h1>
+
+				<p><b>Email:</b><a title="Envie um e-mail" href="mailto:rafaelbarros.df@gmail.com">rafaelbarros.df@gmail.com</a></p>
+				<hr>
+				<p>&copy; - WebCode Rafael Barros, Todos Os Diretiso Reservardos!</p> 
+			</article>
+
+			<div class="clear"></div>
+		</section>
+	</footer>	
 	
 </body>
 </html>
