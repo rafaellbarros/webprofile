04107f600bdd4a1e324bdb71c76be4850c5e0e95
Rafael Barros <rafaelbarros.df@gmail.com>
2015-09-05 22:45:16 -0300

    Finishing last changes

--- index.php
+++ index.php
@@ -19,31 +19,34 @@
 	<header class="container bg-black">
 		<div class="content">
 			<h1 class="fl-left fontzero" >
-				<a title="" href="#index" class="radius">
+				<a title="Rafael" href="#index" class="radius">
 					Rafael Barros
 				</a> 
 			</h1>
 
 			<ul class="main_nav fl-right">
-				<li><a title="About" href="#">About</a></li>
-				<li><a title="Work" href="#">Work</a></li>
-				<li><a title="Contact" href="#">Contact</a></li>
+				<li><a title="Home" href="index.php">Home</a></li>
+				<li><a title="About" href="about.php">About</a></li>
+				<li><a title="Works" href="#works">Works</a></li>
+				<li><a title="Contact" href="contact.php">Contact</a></li>
 			</ul>
 			<div class="clear"></div>
 		</div>
 	</header>
-
 	<!--CONTEUDO-->
 	<article class="container">
 		<div class="content">
 			<h1 class="fontzero">Web Developer</h1>
 				<article class="box box-medium">
 					<h1 class="fontzero">Hi, I'm Rafael a Web Developer.</h1>
-					<p class="fontsize4">Hi, I'm Rafael a Web Developer.</p>
+					<p class="fontsize4">
+									Hi, I'm Rafael 
+									a Web Developer.
+					</p>
 				</article>
-				<article class="main_tec_item_profile box box-medium">
+				<article class="main_tec_item_profile box box-medium fl-right">
 					<h1 class="fontzero">Photo</h1>
-						<img src="img/profile2bw.png" alt="Profile" title="Profile">
+						<img src="img/profile.png" alt="Profile" title="Profile">
 				</article>
 			<div class="clear"></div>
 		</div>
@@ -53,46 +56,51 @@
 			<div class="content">
 				<h1>Abilities</h1>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/html5b-01.png" alt="" title="">
+					<img src="img/html5b-01.png" alt="Html5" title="Html5">
 					<h1>html5</h1>
 				</article>
 				<article class=" main_tec_item box box-small al-center radius">
-					<img src="img/css3b-01.png" alt="" title="">
+					<img src="img/css3b-01.png" alt="Css3" title="Css3">
 					<h1>css3</h1>
 				</article>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/prog-js01b.png" alt="" title="">
+					<img src="img/prog-js01b.png" alt="JavaScript" title="JavaScript">
 					<h1>javascript</h1>
 				</article>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/prog-php01b.png" alt="" title="">
+					<img src="img/prog-php01b.png" alt="PHP" title="PHP">
 					<h1>php</h1>
 				</article>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/dbs-mysqlb.png" alt="" title="">
+					<img src="img/sqlb.png" alt="Sql" title="Sql">
+					<h1>sql</h1>
+				</article>				
+				<article class="main_tec_item box box-small al-center radius">
+					<img src="img/dbs-mysqlb.png" alt="MySql" title="MySql">
 					<h1>mysql</h1>
 				</article>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/gitb.png" alt="" title="">
+					<img src="img/dbs-sqlserverb.png" alt="SqlServer" title="SqlServer">
+					<h1>SqlServer</h1>
+				</article>
+				<article class="main_tec_item box box-small al-center radius">
+					<img src="img/gitb.png" alt="Git" title="Git">
 					<h1>git</h1>
 				</article>				
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/os-linux_1b.png" alt="" title="">
+					<img src="img/os-linux_1b.png" alt="Linux" title="Linux">
 					<h1>linux</h1>
 				</article>
 				<article class="main_tec_item box box-small al-center radius">
-					<img src="img/os-win-04-dark.png" alt="" title="">
+					<img src="img/os-win-04-dark.png" alt="Windows" title="Windows">
 					<h1>windows</h1>
 				</article>															
 				<div class="clear"></div>
 			</div>
 		</div>	
 	</section>
-
-
 	<!--CONTEUDO-->
-
-	<footer class="container bg-light">
+	<footer class="main_footer container bg-black">
 		<section class="content">
 			<h1 class="fontzero">Copyright</h1>
 				<p class="al-center ">&copy; 2015 Rafael Barros.</p> 
