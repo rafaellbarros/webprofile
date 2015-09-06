50cf8f7bd4413c2bad8b8831b208d3cb3f3ef0a6
Rafael Barros <rafaelbarros.df@gmail.com>
2015-09-05 22:33:27 -0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Contact | WebDeveloper</title>
+
+	<!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+	<![endif]-->
+
+	
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/boot.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+ <link rel="shortcut icon" href="img/faviconr.png"/>
+</head>
+<body>
+	<header class="container bg-black">
+		<div class="content">
+			<h1 class="fl-left fontzero" >
+				<a title="Rafael" href="#index" class="radius">
+					Rafael Barros
+				</a> 
+			</h1>
+
+			<ul class="main_nav fl-right">
+				<li><a title="Home" href="index.php">Home</a></li>
+				<li><a title="About" href="about.php">About</a></li>
+				<li><a title="Works" href="#work">Works</a></li>
+				<li><a title="Contact" href="#">Contact</a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</header>
+
+	<!--CONTEUDO-->
+	<article class="container">
+		<div class="main_contact content">
+			<h1 class="fontzero">Web Developer</h1>
+				<article class="box box-medium">
+					<h2>General Infomation</h2>
+					<p class="fontsize1">If you have any questions, suggestions, or if you want to invite me for an interview, 
+						please contact me, will be available to answer any questions. Thank you!</p>
+				</article>
+				<article class="main_tec_item_profile box box-medium">
+					<h2 class="title">Contact Details</h2>
+					<p class="shoticon shoticon-mail"><a title="Envie um E-mail" href="mailto:trafaelbarros.df@gmail.com">rafaelbarros.df@gmail.com</a></p>
+
+					<p class="shoticon shoticon-googleplus"><a title="Google Plus" target="_blank" rel="nofollow" href="https://plus.google.com/+RafaelBarrosBrazil">Google+</a></p>					
+					
+					<p class="shoticon shoticon-github"><a title="Github" target="_blank" rel="nofollow" href="https://github.com/rafaellbarros">Github</a></p>
+					
+					<p class="shoticon shoticon-linkedin"><a title="Linkedin" target="_blank" rel="nofollow" href="https://br.linkedin.com/in/rafaelbarrossilva">Linkedin</a></p>
+
+				</article>
+			<div class="clear"></div>
+		</div>
+	</article>	
+	<!--CONTEUDO-->
+	<footer class="container bg-black ">
+		<section class="content">
+			<h1 class="fontzero">Copyright</h1>
+				<p class="al-center ">&copy; 2015 Rafael Barros.</p> 
+			<div class="clear"></div>
+		</section>
+	</footer>	
+</body>
+</html>
