1c7b72ca5902cd9288c8f76dd3d4509cca62d148
Rafael Barros <rafaelbarros.df@gmail.com>
2015-09-05 22:34:35 -0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>About | WebDeveloper</title>
+
+	<!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+	<![endif]-->
+
+	
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/boot.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+ <link rel="shortcut icon" href="img/faviconr.png"/>
+</head>
+<body>
+	<header class="container bg-black">
+		<div class="content">
+			<h1 class="fl-left fontzero" >
+				<a title="Rafael" href="#index" class="radius">
+					Rafael Barros
+				</a> 
+			</h1>
+
+			<ul class="main_nav fl-right">
+				<li><a title="Home" href="index.php">Home</a></li>
+				<li><a title="About" href="#about">About</a></li>
+				<li><a title="Works" href="#work">Works</a></li>
+				<li><a title="Contact" href="contact.php">Contact</a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</header>
+
+	<!--CONTEUDO-->
+	<article class="container">
+		<div class="content">
+			<h1 class="fontzero">Web Developer</h1>
+				<article class="main_tec_item_profile  box box-small-2">
+					<h2 class="fontzero">Foto</h2>
+					<img src="img/profile2bw.png" alt="Profile" title="Profile">
+				</article>
+				<article class="box box-large-2">
+					<h2>About</h2>
+					<p class="fontsize1">Hi, I'm Rafael Barros! I am 26 years old,I am currently a student of Information Systems,I have the computer technician course, like programming, I try to develop my skills Professional always looking to implement best practices in Software Development, this is my web page, where I will share my web development work when I'm not studying or programming, like playing video games, watching sitcoms and movies, extreme sports (skating, cycling), hanging out with friends, among other things.</p>
+				</article>
+			<div class="clear"></div>
+		</div>
+	</article>	
+	<!--CONTEUDO-->
+	<footer class="container bg-black ">
+		<section class="content">
+			<h1 class="fontzero">Copyright</h1>
+				<p class="al-center ">&copy; 2015 Rafael Barros.</p> 
+			<div class="clear"></div>
+		</section>
+	</footer>	
+</body>
+</html>
